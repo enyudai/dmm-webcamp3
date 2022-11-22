3d0b924d987a94642447a21814595cc84d5ca5df
EC2 Default User <ec2-user@ip-172-31-19-117.ap-northeast-1.compute.internal>
2022-11-22 15:38:43 +0000

    add Bookers2

--- Bookers2/app/controllers/books_controller.rb
+++ Bookers2/app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :ensure_user, only: [:edit, :update, :destroy]
+  
   def index
     @book = Book.new
     @user = current_user
@@ -9,13 +11,23 @@ class BooksController < ApplicationController
     book=Book.new(book_params)
     book.user_id = current_user.id #カラムに情報を代入
     book.save
-    redirect_to book_path(current_user.id)
+    redirect_to book_path(book.id)
   end
   
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update
+    redirect_to book_path(book.id)
   end
   
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    
   end
   
   private
--- Bookers2/app/controllers/users_controller.rb
+++ Bookers2/app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
   
   def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+  end
+  
+  def create
+    book=Book.new(book_params)
+    book.user_id = current_user.id #カラムに情報を代入
+    book.save
+    redirect_to book_path(current_user.id)
   end
   
   def show
-    @user = User.find(params[:id])
-    @book_images = @user.books
+    @user=User.find(params[:id])
+   @books = @user.books
   end
 
   def edit
   end
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  
 end
