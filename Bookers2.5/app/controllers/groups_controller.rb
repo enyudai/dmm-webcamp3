b207e3777720ac54bef2c071fbe698ff31214a86
EC2 Default User <ec2-user@ip-172-31-19-117.ap-northeast-1.compute.internal>
2022-12-08 11:19:23 +0000

    Rails

--- Bookers2.5/app/controllers/groups_controller.rb
+++ Bookers2.5/app/controllers/groups_controller.rb
@@ -1,4 +1,10 @@
 class GroupsController < ApplicationController
+  before_action :specified_user, only: [:edit]
+  
+  def show
+    @group = Group.find(params[:id])
+    @groups = Group.all
+  end
   
   def index
     @groups = Group.all
@@ -10,12 +16,21 @@ class GroupsController < ApplicationController
   if @group.save
     redirect_to groups_path
   else
-    render 'group_form'
+    render :group_form
   end
   end
   
   def edit
     @group = Group.find(params[:id])
+  end
+  
+  def update
+    @group = Group.find(params[:id])
+   if @group.update(group_params)
+    redirect_to groups_path
+   else
+    render :edit
+   end
   end
   
   def group_form
@@ -26,6 +41,11 @@ class GroupsController < ApplicationController
   
   def group_params
     params.require(:group).permit(:name, :introduction, :image)
+  end
+
+  def specified_user
+    @group =  @group = Group.find(params[:id])
+    redirect_to (groups_path) unless @group.owner_id == current_user.id
   end
 
 end
